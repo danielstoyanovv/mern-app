4197b3b6acad5d7107f1e0624e1bd850089b09fb
danielstoyanovv <daniel.stoyanovv@gmail.com>
2024-12-16 09:55:56 +0200

    Add addUsersToCacheMiddleware.ts - implement cache business logic Add addUserToCacheMiddleware.ts - implement cache business logic

---
+++ backend/src/middleware/addUsersToCacheMiddleware.ts
@@ -0,0 +1,24 @@
+"use strict";
+
+import {Request, Response, NextFunction } from "express";
+import { STATUS_SUCCESS } from "../constants/data"
+import {RedisServerService} from "../services/RedisServerService";
+
+export const addUsersToCacheMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const redisClient = new RedisServerService().getRedisClient
+    const cachedData = await redisClient.get("users");
+    if (cachedData) {
+        const users = JSON.parse(cachedData)
+        console.log('Cache hit');
+        return res.status(400).json({
+            status: STATUS_SUCCESS,
+            data: {
+                users,
+                "total" : users.length
+            },
+            message: ""
+        })
+    }
+    console.log('Cache miss');
+    next();
+}
