7c664c8b791d1f5abcbb1881acc296beb06c887e
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-02-24 09:57:06 +0200

    Create TokenManager.ts class - add set token method - add is expired method - add includes Admin method Refactor authMiddleware.ts - implement token manager into the business logic

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,14 +1,15 @@
 "use strict";
 
 import {Request, Response, NextFunction } from "express";
-import {config} from "dotenv"
-config()
+require('dotenv').config();
 import {
     MESSEGE_ERROR,
     STATUS_UNAUTHORIZED,
     STATUS_FORBIDDEN
 } from "../constants/data"
-import jwt from 'jsonwebtoken'
+import {TokenManager} from "../utils/TokenManager";
+
+const tokenManager = new TokenManager()
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token = req.body.token || req.query.token || req.headers['x-access-token'] || false
@@ -21,11 +22,9 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     }
     if (token) {
         try {
-            const payloadBase64 = token.split('.')[1];
-            const decodedJson = Buffer.from(payloadBase64, 'base64').toString();
-            const decoded = JSON.parse(decodedJson)
-            const exp = decoded.exp;
-            const expired = (Date.now() >= exp * 1000)
+            const currentToken = tokenManager
+                .setToken(token)
+            const expired = currentToken.isExpired()
             if (expired) {
                 return res.status(STATUS_FORBIDDEN).json({
                     status: MESSEGE_ERROR,
@@ -33,8 +32,7 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
                     message: "Invalid or expired token."
                 });
             }
-            const tokenData = jwt.verify(token, process.env.JWT_SECRET!, {})
-            const isAdmin = Object.values(tokenData).includes("admin");
+            const isAdmin = currentToken.includesAdmin()
             if (!isAdmin) {
                 return res.status(STATUS_FORBIDDEN).json({
                     status: MESSEGE_ERROR,
